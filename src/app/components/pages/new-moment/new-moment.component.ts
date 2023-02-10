6956a1db830e046cf7a66b17ad1f9934e824fead
Luiz <luizresende09@gmail.com>
2023-02-10 16:58:43 -0400

    primeiro commit

---
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Moment } from 'src/app/Moment';
+import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-new-moment',
+  templateUrl: './new-moment.component.html',
+  styleUrls: ['./new-moment.component.css']
+})
+export class NewMomentComponent implements OnInit {
+  btnText = "Compartilhar"
+
+  constructor(
+    private momentService:MomentService, 
+    private messagesService:MessagesService,
+    private router:Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  async createHandler(moment:Moment){
+     const formData = new FormData();
+
+     formData.append("title", moment.title);
+     formData.append("description", moment.description);
+     
+     if(moment.image){
+      formData.append("image", moment.image);
+     }
+
+     // todo
+     await this.momentService.createMoment(formData).subscribe();
+
+     // enviar para o service
+
+
+     // exibir mensagem
+     this.messagesService.add("Momento adicionado com sucesso!")
+
+     // redirect
+     this.router.navigate(['/']);
+  }
+}
